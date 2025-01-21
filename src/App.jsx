d4fd5fce09cf309df5719c6b0f351dd37221f790
zer0 <max.hasselwander@wiss-edu.ch>
2025-01-21 11:56:59 +0100

    added Login and Auth feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,46 @@
 import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 
-const API = import.meta.env.VITE_API
-const KEY = import.meta.env.VITE_KEY
+const API = import.meta.env.VITE_API;
+const KEY = import.meta.env.VITE_KEY;
 
 const supabase = createClient(API, KEY);
 
 function App() {
-  const [countries, setCountries] = useState([]);
+  const [session, setSession] = useState(null);
 
   useEffect(() => {
-    getCountries();
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const { subscription } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+
+    return () => {
+      subscription?.unsubscribe?.();
+    };
   }, []);
-
-  async function getCountries() {
-    const { data } = await supabase.from("countries").select();
-    setCountries(data);
-  }
-
+  
   return (
-    <ul>
-      {countries.map((country) => (
-        <li key={country.name}>{country.name}</li>
-      ))}
-    </ul>
+    <div>
+      {!session ? (
+        <Auth
+          supabaseClient={supabase}
+          appearance={{ theme: ThemeSupa }}
+          theme="dark"
+        />
+      ) : (
+        <div>
+          <p>Welcome, you are logged in!</p>
+          <button onClick={() => supabase.auth.signOut()}>Sign Out</button>
+        </div>
+      )}
+    </div>
   );
 }
 
